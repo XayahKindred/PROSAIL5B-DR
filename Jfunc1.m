2e2c40394543bc8414f776530a5830920bd155a3
XayahKindred <yangw1663@gmail.com>
2025-10-21 12:12:37 +0800

    Delete Jfunc1.m

--- Jfunc1.m
+++
@@ -1,5 +0,0 @@
-function Jout=Jfunc1(k,l,t)
-del=(k-l)*t;
-Jout(abs(del)>1e-3)=(exp(-l(abs(del)>1e-3)*t)-exp(-k*t))./(k-l(abs(del)>1e-3));
-Jout(abs(del)<=1e-3)=0.5*t*(exp(-k*t)+exp(-l(abs(del)<=1e-3)*t)).*(1-del(abs(del)<=1e-3).*del(abs(del)<=1e-3)/12);
-Jout=transpose(Jout);
